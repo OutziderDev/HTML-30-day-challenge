1e5903e58231320e3cb0171fb644c77b965322a5
Luis <luisbarrios0110@gmail.com>
2025-08-03 22:30:34 -0500

    Feat; Agregando js para el responsive y mejorando los estilos de la caja para menu en version responsive

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-import "./components/NavSidebar.js"
+import "./components/NavSidebar.js";
+
+/* document.addEventListener('DOMContentLoaded', () = {
+  const menuMovil = document.querySelector('.section-menu');
+  menuMovil.addEventListener('click', () = {
+    alert('hola');
+  } )
+}) */
+
+document.addEventListener( 'DOMContentLoaded', () => {
+  const menuMovil = document.querySelector('#btn-menu');
+  const sidebarMenu = document.querySelector('#sidebar');
+
+  /* sidebarMenu.addEventListener('click', () => {
+    alert('lala')
+  }) */
+
+  menuMovil.addEventListener('click', (e) => {
+    sidebarMenu.style.display= 'contents';
+    sidebarMenu.style.zIndex= 90 
+  } )
+})
